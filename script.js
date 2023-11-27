864423c0ba0668f9b8b66fe00a1ee0b36a0c61bd
rilind gashi <rg48828@ubt-uni.net>
2023-11-27 03:46:56 +0100

    commit

--- script.js
+++ script.js
@@ -3,8 +3,10 @@ var pasta = document.querySelector('#pasta')
 var pizza = document.querySelector('#pizza')
 
 const pasta_menu =document.querySelector('.pasta-menu')
-const drikns_menu =document.querySelector('.drinks-menu')
+const drikns_menu =document.querySelector('.drinks')
 const pizza_menu =document.querySelector('.pizza-menu')
+
+var bg_btn = document.querySelectorAll('.cate')
 
 pasta.addEventListener('click', () => {
     pasta_menu.style.display = 'block';
@@ -21,3 +23,8 @@ pizza.addEventListener('click', () => {
     drikns_menu.style.display = 'none';
     pizza_menu.style.display = 'block';
 })
+    bg_btn.forEach(bg_btns => {
+        bg_btns.addEventListener('click', ()=>{
+        (bg_btns.style.backgroundColor = '#808080')? 'red' : '#808080';
+    });
+});
